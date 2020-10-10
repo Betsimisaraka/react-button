ec9c8ec11a8553ac984fa7a5dcdd9585ab84a123
Anita RASOA <anita.ras@onja.org>
2020-10-10 14:30:18 +0300

    Fixing some layout of the button

--- App.js
+++ App.js
@@ -4,38 +4,38 @@ import Buttons from './Buttons'
 
 function App() {
     return  (
-        <div>
+        <div className="main">
             <div className="section">
-                <p>Button</p>
-                <Buttons>
+                <p>{`<Button />`}</p>
+                <Buttons color="default">
                     Default
                 </Buttons>
             </div>
             <div className="section">
-                <p>Button variant="outline"</p>
+                <p>{`<Button variant="outline" />`}</p>
                 <Buttons variant="outline">
                     Default
                 </Buttons>
             </div>
             <div className="section">
-                <p>Button variant="text"</p>
+            <p>{`<Button variant="text" />`}</p>
                 <Buttons variant="text">
                     Default
                 </Buttons>
             </div>
             <div className="section">
-                <p>Button disabledShadow</p>
-                <Buttons disabledShadow>Default</Buttons>
+                <p>{`<Button disabledShadow />`}</p>
+                <Buttons disabledShadow color="primary">Default</Buttons>
             </div>
             <div className="wrapper">
                 <div className="section">
-                    <p>Button disabled</p>
+                    <p>{`<Button disabled />`}</p>
                     <Buttons disabled>
                         Disabled
                     </Buttons>
                 </div>
                 <div className="section">
-                    <p>Button variant="text" disabled</p>
+                    <p>{`<Button variant="text" disabled />`}</p>
                     <Buttons variant="text" disabled>
                         Disabled
                     </Buttons>
@@ -43,59 +43,59 @@ function App() {
             </div>
             <div className="wrapper">
                 <div className="section">
-                    <p>Button startIcon="local_grocery_store"</p>
-                    <Buttons startIcon="local_grocery_store">
+                    <p>{`<Button startIcon="local_grocery_store" />`}</p>
+                    <Buttons startIcon="local_grocery_store" color="primary">
                         Default
                     </Buttons>
                 </div>
                 <div className="section">
-                    <p>Button endIcon="local_grocery_store"</p>
-                    <Buttons endIcon="local_grocery_store">
-                        Default
-                    </Buttons>
-                </div>
-            </div>
-            <div className="wrapper">
-                <div className="section">
-                    <p>Button size="sm"</p>
-                    <Buttons size="sm">
-                        Default
-                    </Buttons>
-                </div>
-                <div className="section">
-                    <p>Button size="md"</p>
-                    <Buttons size="md">
-                        Default
-                    </Buttons>
-                </div>
-                <div className="section">
-                    <p>Button size="lg"</p>
-                    <Buttons size="lg">
+                    <p>{`<Button endIcon="local_grocery_store" />`}</p>
+                    <Buttons endIcon="local_grocery_store" color="primary">
                         Default
                     </Buttons>
                 </div>
             </div>
             <div className="wrapper">
                 <div className="section">
-                    <p>Button color="default"</p>
+                    <p>{`<Button size="sm" />`}</p>
+                    <Buttons size="sm" color="primary">
+                        Default
+                    </Buttons>
+                </div>
+                <div className="section">
+                    <p>{`<Button size="md" />`}</p>
+                    <Buttons size="md" color="primary">
+                        Default
+                    </Buttons>
+                </div>
+                <div className="section">
+                    <p>{`<Button size="lg" />`}</p>
+                    <Buttons size="lg" color="primary">
+                        Default
+                    </Buttons>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="section">
+                    <p>{`<Button color="default" />`}</p>
                     <Buttons color="default">
                         Default
                     </Buttons>
                 </div>
                 <div className="section">
-                    <p>Button color="primary"</p>
+                    <p>{`<Button color="primary" />`}</p>
                     <Buttons color="primary">
                         Default
                     </Buttons>
                 </div>
                 <div className="section">
-                    <p>Button color="secondary"</p>
+                    <p>{`<Button color="secondary" />`}</p>
                     <Buttons color="secondary">
                         Secondary
                     </Buttons>
                 </div>
                 <div className="section">
-                    <p>Button color="danger"</p>
+                    <p>{`<Button color="danger" />`}</p>
                     <Buttons color="danger">
                     Danger
                     </Buttons>
--- Buttons.js
+++ Buttons.js
@@ -1,38 +1,41 @@
 import React from 'react'
 
 function Buttons(props) {
+    let defaultBtn = props.color === "default" ? "default" : ""
     let variantOutline = props.variant === "outline" ? "outline" : ""
     let variantText = props.variant === "text" ? "text" : ""
-    let disabledShadow = props.disabledShadow ? "disabledshadow" : ""
+    let disabledShadow = props.disabledShadow && props.color === "primary" ? "disabledshadow" : ""
     let disabled = props.disabled ? "disabled" : ""
     let disabledText = props.disabled && props.variant === "text" ? "disabledText" : ""
-    let smSize = props.size === "sm" ? "small" : ""
-    let mdSize = props.size === "md" ? "medium" : ""
-    let lgSize = props.size === "lg" ? "large" : ""
+    let smSize = props.size === "sm" && props.color === "primary" ? "small" : ""
+    let mdSize = props.size === "md" && props.color === "primary" ? "medium" : ""
+    let lgSize = props.size === "lg" && props.color === "primary" ? "large" : ""
     let primaryBtn = props.color === "primary" ? "primary" : ""
     let secondaryBtn = props.color === "secondary" ? "secondary" : ""
     let dangerBtn = props.color === "danger" ? "danger" : ""
-    let icons = <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 9c.55 0 1-.45 1-1V6h2c.55 0 1-.45 1-1s-.45-1-1-1h-2V2c0-.55-.45-1-1-1s-1 .45-1 1v2H9c-.55 0-1 .45-1 1s.45 1 1 1h2v2c0 .55.45 1 1 1zm-5 9c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2zm-8.9-5h7.45c.75 0 1.41-.41 1.75-1.03l3.38-6.13c.27-.48.09-1.09-.39-1.36-.48-.26-1.09-.09-1.35.39L15.55 11H8.53L4.54 2.57c-.16-.35-.52-.57-.9-.57H2c-.55 0-1 .45-1 1s.45 1 1 1h1l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h11c.55 0 1-.45 1-1s-.45-1-1-1H7l1.1-2z" fill="#FFFFFF"/></svg>;
-    let startIcons = props.startIcon === "local_grocery_store" ? icons : ""
-    let startIconClass = props.startIcon === "local_grocery_store" ? "startIcon" : ""
-    let endIcons = props.endIcon === "local_grocery_store" ? icons : ""
-    let endIconClass = props.endIcon === "local_grocery_store" ? "endIcon" : ""
+    let icon = <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 9c.55 0 1-.45 1-1V6h2c.55 0 1-.45 1-1s-.45-1-1-1h-2V2c0-.55-.45-1-1-1s-1 .45-1 1v2H9c-.55 0-1 .45-1 1s.45 1 1 1h2v2c0 .55.45 1 1 1zm-5 9c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2zm-8.9-5h7.45c.75 0 1.41-.41 1.75-1.03l3.38-6.13c.27-.48.09-1.09-.39-1.36-.48-.26-1.09-.09-1.35.39L15.55 11H8.53L4.54 2.57c-.16-.35-.52-.57-.9-.57H2c-.55 0-1 .45-1 1s.45 1 1 1h1l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h11c.55 0 1-.45 1-1s-.45-1-1-1H7l1.1-2z" fill="#FFFFFF"/></svg>;
+    let startIcons = props.startIcon === "local_grocery_store" ? icon : ""
+    let startIconClass = props.startIcon === "local_grocery_store" && props.color === "primary" ? "startIcon" : ""
+    let endIcons = props.endIcon === "local_grocery_store" ? icon : ""
+    let endIconClass = props.endIcon === "local_grocery_store" && props.color === "primary" ? "endIcon" : ""
     return (
             <button className={`
-                ${variantOutline}
-                ${variantText}
-                ${disabledShadow}
-                ${disabled}
-                ${disabledText}
-                ${smSize}
-                ${mdSize}
-                ${lgSize}
-                ${primaryBtn}
-                ${secondaryBtn}
-                ${dangerBtn}
-                ${startIconClass}
-                ${endIconClass}
-            `}>{startIcons} {props.children} {endIcons}</button>
+                                ${defaultBtn}
+                                ${variantOutline}
+                                ${variantText}
+                                ${disabledShadow}
+                                ${disabled}
+                                ${disabledText}
+                                ${smSize}
+                                ${mdSize}
+                                ${lgSize}
+                                ${primaryBtn}
+                                ${secondaryBtn}
+                                ${dangerBtn}
+                                ${startIconClass}
+                                ${endIconClass}
+                            `}>
+                        {startIcons} {props.children} {endIcons}</button>
     ) 
 }
 
